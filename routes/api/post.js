3ae06580ea35ec03f192257a5577550e80a9d8e2
Mahbubur Rahman <mahbubur2310@gmail.com>
2022-08-02 20:18:08 +0600

    Get All Post Api Completed

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 const Posts = require("../../models/post");
 
-// Post api for Create Post
+// Get All Post
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    if (!posts) throw Error("No Items");
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+});
 
+// Post api for Create Post
 router.post("/", async (req, res) => {
   const newPost = new Posts(req.body);
 
@@ -11,6 +21,17 @@ router.post("/", async (req, res) => {
     const post = await newPost.save();
     if (!post) throw Error("Something went wrong while saving post");
     res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+});
+
+// Delete Post by api/posts/:id
+router.delete("/:id", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    if (!posts) throw Error("No Items");
+    res.status(200).json(posts);
   } catch (err) {
     res.status(400).json({ msg: err });
   }
