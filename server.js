88a7cefd61cdd6839ff2622f89ae47724aeabc00
Md.Mahbubur Rahman <76196689+DeveloperMahbub@users.noreply.github.com>
2022-08-02 21:52:17 +0600

    Update server.js
    Completed Rest Api

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const mongose = require("mongoose");
 const { MONGO_URI } = require("./config");
 
+// For Cors
+const cors = require("cors");
 //Routes
 const PostsRoutes = require("./routes/api/post");
 const app = express();
+app.use(cors());
 //Bodyparser Middleware
 app.use(express.json());
 //Connect to MongoDB
